e16a10b5997a9a2250f6295b2068e2b3dcc1eb57
eunsung <dmsqls6789@naver.com>
2023-02-12 23:53:53 +0900

    ~297

---
+++ SubmitValue/SubmitValue/ResultViewController.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+}
--- SubmitValue/SubmitValue/ViewController.swift
+++ SubmitValue/SubmitValue/ViewController.swift
@@ -4,16 +4,37 @@
 //
 //  Created by EUNSUNG on 2023/02/12.
 //
-//s df
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var isUpdate: UISwitch!
+    
+    @IBOutlet var interval: UIStepper!
+    
+    @IBOutlet var isUpdqteText: UILabel!
+    
+    @IBOutlet var intervalText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true) {
+            self.isUpdqteText.text = "갱신함"
+        } else {
+            self.isUpdqteText.text = "갱신하지 않음"
+        }
+    }
+    
+    @IBAction func onStepper(_ sender: UIStepper) {
+        let value = Int(sender.value)
+        self.intervalText.text = "\(value) 분 마다"
+    }
 }
 
