f6d4dd6dc5de9ceb30c5ceaca553a7d5b09c6b31
eunsung <dmsqls6789@naver.com>
2023-02-13 23:34:05 +0900

    ResultViewController - onBack()

--- SubmitValue/SubmitValue/ResultViewController.swift
+++ SubmitValue/SubmitValue/ResultViewController.swift
@@ -17,4 +17,8 @@ class ResultViewController: UIViewController {
         self.resultUpdate.text = (self.paramUpdate == true ? "자동갱신" : "자동갱신안함")
         self.resultInterval.text = "\(Int(paramInterval)) 분 마다 갱신"
     }
+    
+    @IBAction func onBack(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
 }
