3683b971bd952e322306a692a0101d81d86259d9
eunsung <dmsqls6789@naver.com>
2023-02-13 23:24:17 +0900

    onSubmit

--- SubmitValue/SubmitValue/ViewController.swift
+++ SubmitValue/SubmitValue/ViewController.swift
@@ -36,5 +36,21 @@ class ViewController: UIViewController {
         let value = Int(sender.value)
         self.intervalText.text = "\(value) 분 마다"
     }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        // VC2 인스턴스 생성
+        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "RVC") as? ResultViewController else {
+            
+            return
+        }
+        
+        // 값을 전달하는 과정을 여기에 작성
+        rvc.paramEmail = self.email.text!
+        rvc.paramUpdate = self.isUpdate.isOn
+        rvc.paramInterval = self.interval.value
+        
+        // 화면 이동
+        self.present(rvc, animated: true)
+    }
 }
 
