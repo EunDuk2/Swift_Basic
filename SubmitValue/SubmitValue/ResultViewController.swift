b4ccba03c017b616f5564ff4deb8454ce721f6e2
eunsung <dmsqls6789@naver.com>
2023-02-13 23:32:41 +0900

    ResultViewController - viewDidLoad()

--- SubmitValue/SubmitValue/ResultViewController.swift
+++ SubmitValue/SubmitValue/ResultViewController.swift
@@ -11,4 +11,10 @@ class ResultViewController: UIViewController {
     var paramEmail:String = ""
     var paramUpdate:Bool = false
     var paramInterval:Double = 0
+    
+    override func viewDidLoad() {
+        self.resultEmail.text = paramEmail
+        self.resultUpdate.text = (self.paramUpdate == true ? "자동갱신" : "자동갱신안함")
+        self.resultInterval.text = "\(Int(paramInterval)) 분 마다 갱신"
+    }
 }
