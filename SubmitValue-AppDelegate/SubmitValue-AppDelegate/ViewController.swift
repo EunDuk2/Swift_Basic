80b837a3dd29c6187d18f74dd1522133d8141df7
eunsung <dmsqls6789@naver.com>
2023-02-15 21:46:08 +0900

    ~340

---
+++ SubmitValue-AppDelegate/SubmitValue-AppDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmitValue-AppDelegate
+//
+//  Created by EUNSUNG on 2023/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
