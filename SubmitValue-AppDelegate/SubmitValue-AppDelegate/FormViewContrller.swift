c5b64bd3fbd1c65f3f34adb91081ec9b014c2ea5
eunsung <dmsqls6789@naver.com>
2023-02-15 23:32:13 +0900

    ~343

--- SubmitValue-AppDelegate/SubmitValue-AppDelegate/FormViewContrller.swift
+++ SubmitValue-AppDelegate/SubmitValue-AppDelegate/FormViewContrller.swift
@@ -2,6 +2,22 @@ import UIKit
 
 class FormViewController: UIViewController {
     
+    @IBOutlet var email: UITextField!
     
+    @IBOutlet var isUpdate: UISwitch!
     
+    @IBOutlet var interval: UIStepper!
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        // AppDelegate 객체의 인스턴스를 가져온다
+        let ad = UIApplication.shared.delegate as? AppDelegate
+        
+        // 값을 저장한다
+        ad?.paramEmail = self.email.text
+        ad?.paramUpdate = self.isUpdate.isOn
+        ad?.paramInterval = self.interval.value
+        
+        // 이전 화면으로 복귀한다.
+        self.presentingViewController?.dismiss(animated: true)
+    }
 }
--- SubmitValue-AppDelegate/SubmitValue-AppDelegate/ViewController.swift
+++ SubmitValue-AppDelegate/SubmitValue-AppDelegate/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var resultEmail: UILabel!
+    
+    @IBOutlet var resultUpdate: UILabel!
+    
+    @IBOutlet var resultinterval: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // AppDelegate 객체의 인스턴스를 가져온다.
+        let ad = UIApplication.shared.delegate as? AppDelegate
+        
+        if let email = ad?.paramEmail {
+            resultEmail.text = email
+        }
+        if let update = ad?.paramUpdate {
+            resultUpdate.text = update==true ? "자동갱신" : "자동갱신안함"
+        }
+        if let interval = ad?.paramInterval {
+            resultinterval.text = "\(Int(interval))"
+        }
     }
-
-
 }
 
